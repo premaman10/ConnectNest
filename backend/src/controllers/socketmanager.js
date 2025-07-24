3e28778139fe64865014ec77357ebefa83e7f078
sakshi <119154881+premaman10@users.noreply.github.com>
2025-07-24 09:40:05 +0530

    Add files Via Upload

--- backend/src/controllers/socketmanager.js
+++ backend/src/controllers/socketmanager.js
@@ -8,83 +8,76 @@ let messages = {}
 
 let timeOnline  = {}
 
-export const connectToSocket = (server)=>{
+export const connectToSocket = (server) => {
     const io = new Server(server, {
-    cors: {
-        origin: ["http://localhost:3000", "https://connectnestfrontend.onrender.com"],
-        methods: ["GET", "POST"],
-        credentials: true
-    }
-});
+        cors: {
+            origin: ["http://localhost:3000", "https://connectnestfrontend.onrender.com"],
+            methods: ["GET", "POST"],
+            credentials: true
+        }
+    });
 
+    let connections = {};
+    let messages = {};
+    let timeOnline = {};
 
-    io.on("connection",(socket)=>{
-        socket.on("join-call",(path)=>{
-            if(connections[path]===undefined){
-                connections[path] = []
-            }
-            connections[path].push(socket.id)
+    io.on("connection", (socket) => {
+        socket.on("join-call", (room) => {
+            if (!connections[room]) connections[room] = [];
+            connections[room].push(socket.id);
             timeOnline[socket.id] = new Date();
-            
 
-            for(let a= 0;a<connections[path].length;a++){
-                io.to(connections[path][a]).emit("user-joined",socket.id,connections[path])
+            connections[room].forEach(id => {
+                io.to(id).emit("user-joined", socket.id, connections[room]);
+            });
+
+            if (messages[room]) {
+                messages[room].forEach(msg => {
+                    io.to(socket.id).emit("chat-message", msg.data, msg.sender, msg["socket-id-sender"]);
+                });
             }
-            if(messages[path]!==undefined){
-                for(let a = 0;a<messages[path].length;++a){
-                    io.to(socket.id).emit("chat-message",messages[path][a]['data'],
-                        messages[path][a]['sender'],messages[path][a]['socket-id-sender'])
-                }
+        });
+
+        socket.on("signal", (toId, message) => {
+            io.to(toId).emit("signal", socket.id, message);
+        });
+
+        socket.on("chat-message", (data, sender) => {
+            const [room, found] = Object.entries(connections).find(([_, users]) => users.includes(socket.id)) || ["", false];
+            if (found) {
+                if (!messages[room]) messages[room] = [];
+                messages[room].push({ data, sender, "socket-id-sender": socket.id });
+                connections[room].forEach(id => {
+                    io.to(id).emit("chat-message", data, sender, socket.id);
+                });
             }
-        })
-        socket.on("signal",(toId, message)=>{
-            io.to(toId).emit("signal",socket.id,message);
-        })
-        socket.on("chat-message",(data,sender)=>{
-            const [matchingRoom, found] = Object.entries(connections)
-            .reduce(([room,isFound],[roomKey,roomValue])=>{
-                if(!isFound && roomValue.includes(socket.id)){
-                    return [roomKey,true];
-                }
-                return [room,isFound];
-            },['',false]);
+        });
 
+        socket.on("disconnect", () => {
+            let roomFound = null;
 
-            if(found ===true){
-                if(messages[matchingRoom]===undefined){
-                    messages[matchingRoom] = []
-                }
-                messages[matchingRoom].push({'data':data,'sender':sender,'socket-id-sender':socket.id})
-                console.log("message:", sender, data)
-                connections[matchingRoom].forEach((elem)=>{
-                    io.to(elem).emit("chat-message",data,sender,socket.id)
-                })
-            }
-        })
-        socket.on("disconnect",()=>{
-                var diffTime = Math.abs(timeOnline[socket.id]- new Date())
-                var key 
-                for(const[k,v]of JSON.parse(JSON.stringify(Object.entries(connections)))){
-                    for(let a = 0;a<v.length;++a){
-                        if(v[a]==socket.id){
-                            key = k
+            for (const [room, users] of Object.entries(connections)) {
+                const index = users.indexOf(socket.id);
+                if (index !== -1) {
+                    roomFound = room;
+                    users.splice(index, 1);
+                    connections[room] = users;
+                    connections[room].forEach(id => {
+                        io.to(id).emit("user-left", socket.id);
+                    });
 
-                            for(let b = 0;b<connections[key].length;++b){
-                                io.to(connections[key][a]).emit('user-left',socket.id)
-                            }
-
-                            var index = connections[key].indexOf(socket.id)
-                            connections[key].splice(index,1)
-
-                            if(connections[key].length==0){
-                                delete connections[key]
-                            }
-                        }
+                    if (connections[room].length === 0) {
+                        delete connections[room];
+                        delete messages[room];
                     }
+                    break;
                 }
-        })
-    })
+            }
+
+            delete timeOnline[socket.id];
+        });
+    });
 
     return io;
-}
+};
 // export default connectToSocket;
--- frontend/src/environment.js
+++
@@ -1,6 +0,0 @@
-let IS_PROD = true; // Changed to true for production deployment
-const server = IS_PROD ?
-    "https://connectnest.onrender.com" :
-    "http://localhost:5000"
-
-export default server;
